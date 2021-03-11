20f0581cca8279b4d7f4dc5d83c1d318cde86585
Stephen Corwin <stephenjcorwin@gmail.com>
2021-03-11 14:22:39 -0500

    Fixed small bug with Vanilla-Plus

--- 📦 Vanilla-Plus.bat
+++ 📦 Vanilla-Plus.bat
@@ -2,5 +2,3 @@ rmdir /s/q "../BepInEx/config"
 rmdir /s/q "../BepInEx/plugins"
 ROBOCOPY "./packs/common/config" "../BepInEx/config" /E
 ROBOCOPY "./packs/common/plugins" "../BepInEx/plugins" /E
-ROBOCOPY "./packs/Vanilla/config" "../BepInEx/config" /E
-ROBOCOPY "./packs/Vanilla/plugins" "../BepInEx/plugins" /E
